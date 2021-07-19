49618ddb9e992211d05cf952f8c3ddb03f4e5dc4
edy527 <edgardobeltran527@gmail.com>
2021-07-19 17:06:25 -0300

    Primer commit, se agrega titulo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>
+          Productos estéticos 
+        </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
